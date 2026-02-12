e117d4bb1661dad977297f58df54da448292e44c
Jan Floyd Benedict <135410007+BennyCodes26@users.noreply.github.com>
2026-02-12 06:58:22 +0000

    Fix: implemented random default color for categories

--- addons_library/library_extensions/models/library_book_category.py
+++ addons_library/library_extensions/models/library_book_category.py
@@ -7,7 +7,10 @@ class LibraryBookCategory(models.Model):
 
     name = fields.Char(string="Name", required=True)
     # Added to fix error when adding category for adding a book entry.
-    color = fields.Integer(string="Color Index")
+    color = fields.Integer(
+        string="Color Index",
+        default=lambda self: random.randint(1, 11)
+        )
 
     _sql_constraints = [
         ('name_unique', 'unique(name)', 'The category name must be unique!')
